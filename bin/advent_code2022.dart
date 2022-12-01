6a208cf2cb3a62076593dbf0b75b0aebd836dbe1
ABHISHEK PATIL <abhishek.patil@joflee.com>
2022-12-01 21:49:51 +0530

    comparison program

--- bin/advent_code2022.dart
+++ bin/advent_code2022.dart
@@ -1,26 +1,19 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:math';
 
 void main(List<String> arguments) {
-  File('input.txt').readAsString().then((String data) {
+  File('input2021.txt').readAsString().then((String data) {
     final jsonData = json.encode(data).replaceAll(r'"', '');
-    final x = jsonData.split(r'\n\n');
-    final result = x
-        .map(
-          (e) => stringSum(e.split(r'\n')),
-        )
-        .toList();
+    final x = jsonData.split(r'\n');
+    int result = 0;
+    for (var i = 2; i < x.length - 1; i++) {
+      final prev = int.parse(x[i - 2]) + int.parse(x[i - 1]) + int.parse(x[i]);
+      final current =
+          int.parse(x[i - 1]) + int.parse(x[i]) + int.parse(x[i + 1]);
+      if (current > prev) {
+        result += 1;
+      }
+    }
     print(result);
-    final mx = result.reduce(max);
-    print('max is : $mx & its index is: ${result.indexOf(mx)}');
   });
-}
-
-int stringSum(List<String> data) {
-  int result = 0;
-  for (final i in data) {
-    result += int.parse(i);
-  }
-  return result;
 }
