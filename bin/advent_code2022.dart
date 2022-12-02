f38fa557ac2e4b5723ba416172fd4f928d2ce5a6
ABHISHEK PATIL <abhishek.patil@joflee.com>
2022-12-02 20:47:16 +0530

    rock paper scissor implemented

--- bin/advent_code2022.dart
+++ bin/advent_code2022.dart
@@ -4,23 +4,58 @@ import 'dart:io';
 void main(List<String> arguments) {
   File('input.txt').readAsString().then((String data) {
     final jsonData = json.encode(data).replaceAll(r'"', '');
-    final x = jsonData.split(r'\n\n');
-    final result = x
-        .map(
-          (e) => stringSum(e.split(r'\n')),
-        )
-        .toList();
-    // final mx = result.reduce(max);
-    // print('max is : $mx & its index is: ${result.indexOf(mx)}');
-    result.sort((a, b) => b.compareTo(a));
-    print(result);
+    final allMatches = jsonData.split(r'\n');
+    int totalScore = 0;
+    for (final match in allMatches) {
+      final res = score(match);
+      totalScore += res;
+    }
+
+    print(totalScore);
   });
 }
 
-int stringSum(List<String> data) {
-  int result = 0;
-  for (final i in data) {
-    result += int.parse(i);
+int score(String data) {
+  String opponentMove = data.split(r' ')[0];
+  String yourMove = data.split(r' ')[1];
+  int choicePoints = points[yourMove] ?? 0;
+  final result = opponentMove.match(yourMove);
+  print(
+      'Match($opponentMove $yourMove): $choicePoints + $result = ${choicePoints + result}');
+  choicePoints += result;
+  return choicePoints;
+}
+
+const points = {
+  'X': 1,
+  'Y': 2,
+  'Z': 3,
+};
+
+extension RockPaperScissor on String {
+  int match(String me) {
+    final opp = this;
+
+    // rock
+    if (opp == 'A') {
+      if (me == 'X') return 3; // rock - draw
+      if (me == 'Y') return 6; // paper - won
+      if (me == 'Z') return 0; // scissor - loos
+    }
+
+    // paper
+    if (opp == 'B') {
+      if (me == 'X') return 0; // rock - loose
+      if (me == 'Y') return 3; // paper - draw
+      if (me == 'Z') return 6; // scissor - won
+    }
+
+    // scissor
+    if (opp == 'C') {
+      if (me == 'X') return 6; // rock - won
+      if (me == 'Y') return 0; // paper - loose
+      if (me == 'Z') return 3; // scissor - draw
+    }
+    return 0;
   }
-  return result;
 }
