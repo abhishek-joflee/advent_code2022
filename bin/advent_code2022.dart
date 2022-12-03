cecc21004232c593275f26717fffc19881c61223
ABHISHEK PATIL <abhishek.patil@joflee.com>
2022-12-03 14:46:25 +0530

    rucksack error found

--- bin/advent_code2022.dart
+++ bin/advent_code2022.dart
@@ -4,56 +4,35 @@ import 'dart:io';
 void main(List<String> arguments) {
   File('input.txt').readAsString().then((String data) {
     final jsonData = json.encode(data).replaceAll(r'"', '');
-    final allMatches = jsonData.split(r'\n');
+    final allRucksack = jsonData.split(r'\n');
     int totalScore = 0;
-    for (final match in allMatches) {
-      final res = score(match);
+    for (final ruck in allRucksack) {
+      final res = priority(
+        ruck.substring(0, ruck.length ~/ 2),
+        ruck.substring(ruck.length ~/ 2),
+      );
       totalScore += res;
     }
-
     print(totalScore);
   });
 }
 
-int score(String data) {
-  String opponentMove = data.split(r' ')[0];
-  String yourMove = data.split(r' ')[1];
-  int choicePoints = points[yourMove] ?? 0;
-  final result = opponentMove.match(yourMove);
-  choicePoints += result;
-  return choicePoints;
+int priority(String data1, String data2) {
+  final data1Ascii = getMyAscii(data1.runes);
+  final data2Ascii = getMyAscii(data2.runes);
+
+  for (final i in data1Ascii) {
+    if (data2Ascii.contains(i)) {
+      return i;
+    }
+  }
+  return 0;
 }
 
-const points = {
-  'X': 0,
-  'Y': 3,
-  'Z': 6,
-};
-
-extension RockPaperScissor on String {
-  int match(String me) {
-    final opp = this;
-
-    // rock
-    if (opp == 'A') {
-      if (me == 'X') return 3; // loose - scissor
-      if (me == 'Y') return 1; // draw - rock
-      if (me == 'Z') return 2; // win - paper
-    }
-
-    // paper
-    if (opp == 'B') {
-      if (me == 'X') return 1; // loose - rock
-      if (me == 'Y') return 2; // draw - paper
-      if (me == 'Z') return 3; // win - scissor
-    }
-
-    // scissor
-    if (opp == 'C') {
-      if (me == 'X') return 2; // loose - paper
-      if (me == 'Y') return 3; // draw - scissor
-      if (me == 'Z') return 1; // win - rock
-    }
-    return 0;
-  }
+List<int> getMyAscii(Runes runes) {
+  return runes.map((e) {
+    final x = e - 96;
+    if (x > 0) return x;
+    return x + 58;
+  }).toList();
 }
